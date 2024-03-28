0e49dbcc408df57db3282ac4fef3ebc82da9eb56
Methuselah-Nwodobeh <methuselah.nwodobeh@amalitech.com>
2024-03-28 14:54:24 +0000

    finished three labs

---
+++ src/main/java/com/amalitech/upskilling/OutPut.java
@@ -0,0 +1,27 @@
+package com.amalitech.upskilling;
+
+public class OutPut {
+    private OutPut() {
+    }
+
+    // ANSI escape codes for text colors
+    public static class Colors {
+        private Colors() {
+        }
+
+        public static final String RESET = "\u001B[0m";
+        public static final String BLACK = "\u001B[30m";
+        public static final String RED = "\u001B[31m";
+        public static final String GREEN = "\u001B[32m";
+        public static final String YELLOW = "\u001B[33m";
+        public static final String BLUE = "\u001B[34m";
+        public static final String PURPLE = "\u001B[35m";
+        public static final String CYAN = "\u001B[36m";
+        public static final String WHITE = "\u001B[37m";
+    }
+
+    // Example method to print colored text
+    public static void printColoredText(String text, String color) {
+        System.out.println(color + text + Colors.RESET);
+    }
+}
